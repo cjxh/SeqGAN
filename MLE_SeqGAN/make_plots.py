ed2217cff8a09a1d1c17dc36114b7d2f5b3ef58c
Christina Hung <cjh@cs.stanford.edu>
2017-03-06 01:21:46 +0000

    probably wrong learning curves

--- MLE_SeqGAN/make_plots.py
+++ MLE_SeqGAN/make_plots.py
@@ -6,7 +6,7 @@ import numpy as np
   
 # Read the data into a pandas DataFrame.      
 mle_data = cPickle.load(open('save/mle-loss-20170304-015334.pkl'))
-seqgan_data = cPickle.load(open('save/seqgan-loss-20170305-085624.pkl'))
+seqgan_data = cPickle.load(open('save/seqgan-loss-20170305-232119.pkl'))
 
 # These are the "Tableau 20" colors as RGB.    
 tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
@@ -37,7 +37,8 @@ ax.spines["left"].set_visible(False)
 ax.get_xaxis().tick_bottom()    
 ax.get_yaxis().tick_left()  
 print mle_data[0,0], mle_data[-1,0]
-xmin, xmax, ymin, ymax = min(mle_data[0, 0], seqgan_data[0,0]), max(mle_data[-1, 0], seqgan_data[-1,0]), 8.5, 10.5  
+print seqgan_data[0,0], seqgan_data[-1, 0]
+xmin, xmax, ymin, ymax = min(mle_data[0, 0], seqgan_data[0,0]), max(mle_data[-1,0],seqgan_data[-1,0]), 8.5, 10.5  
   
 # Limit the range of the plot to only where the data is.    
 # Avoid unnecessary whitespace.    
