ca4e0a80210645f376ac7ed45bb3d3e77c92b7a4
Brendan Corcoran <BrendanCorcoran@DN525av0.SUNet>
2017-03-17 16:46:49 -0700

    disc training

--- MaLi_GAN/plot_acc.py
+++ MaLi_GAN/plot_acc.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import cPickle 
 import numpy as np
 
-acc = cPickle.load(open('./pretrain_losses.txt'))
+acc = cPickle.load(open('./pretrain_perplexities_eval.txt'))
+acc2 = cPickle.load(open('./eval_perps.txt'))
+acc = np.concatenate((acc, acc2))
 
 plt.plot(range(len(acc)), acc)
 
