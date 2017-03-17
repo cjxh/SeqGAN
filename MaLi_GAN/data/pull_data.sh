99ae054a861d46143dda65bf34080960713a669d
Christina Hung <cjh@cs.stanford.edu>
2017-03-16 21:21:36 -0700

    added preprocessed dev/test sets

--- MaLi_GAN/data/pull_data.sh
+++ MaLi_GAN/data/pull_data.sh
@@ -1,3 +1,5 @@
+wget -c 'https://www.dropbox.com/s/u9tc919ygyycr8k/preprocessed_test_sentences.txt?dl=0<Paste>' -O preprocessed_test_sentences.txt
+wget -c 'https://www.dropbox.com/s/yaevnhrdfubrf15/preprocessed_dev_sentences.txt?dl=0' -O preprocessed_dev_sentences.txt
 wget -c 'https://www.dropbox.com/s/qrlq98lss18c1lq/preprocessed_train_sentences.txt?dl=0' -O preprocessed_train_sentences.txt
 wget -c 'https://www.dropbox.com/s/k8wzj80eulu3qjj/trimmed_glove_vectors.npy?dl=0' -O trimmed_glove_vectors.npy
 wget -c 'https://www.dropbox.com/s/cpkx9i4wt0s80vr/trimmed_word_lexicon.txt?dl=0' -O trimmed_word_lexicon.txt
