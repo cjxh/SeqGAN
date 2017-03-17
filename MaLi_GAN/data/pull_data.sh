ea77fd719aaf59191185c21ecfb707f76abb37e1
Christina Hung <cjh@cs.stanford.edu>
2017-03-17 01:42:15 +0000

    updated data_preprocess

---
+++ MaLi_GAN/data/pull_data.sh
@@ -0,0 +1,3 @@
+wget -c 'https://www.dropbox.com/s/25ish983q0fwxoj/train_sentences.conll?dl=0' -0 train_sentences.conll
+wget -c 'https://www.dropbox.com/s/k8wzj80eulu3qjj/trimmed_glove_vectors.npy?dl=0' -0 trimmed_glove_vectors.npy
+wget -c 'https://www.dropbox.com/s/cpkx9i4wt0s80vr/trimmed_word_lexicon.txt?dl=0' -0 trimmed_word_lexicon.txt
