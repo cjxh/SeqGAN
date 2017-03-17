6b3034ad9137ea4bf2f75cf493649af34de167b7
Christina Hung <cjh@cs.stanford.edu>
2017-03-16 19:19:40 -0700

    fixed data preprocessing

--- MaLi_GAN/data/pull_data.sh
+++ MaLi_GAN/data/pull_data.sh
@@ -1,2 +1,3 @@
+wget -c 'https://www.dropbox.com/s/qrlq98lss18c1lq/preprocessed_train_sentences.txt?dl=0' -O preprocessed_train_sentences.txt
 wget -c 'https://www.dropbox.com/s/k8wzj80eulu3qjj/trimmed_glove_vectors.npy?dl=0' -O trimmed_glove_vectors.npy
 wget -c 'https://www.dropbox.com/s/cpkx9i4wt0s80vr/trimmed_word_lexicon.txt?dl=0' -O trimmed_word_lexicon.txt
