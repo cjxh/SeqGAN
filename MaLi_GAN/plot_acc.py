0ad198924f5444238eca31e27b59f2579cdf6aa8
Brendan Corcoran <BrendanCorcoran@DN0a22b17a.SUNet>
2017-03-16 18:40:16 -0700

    added perplexity

--- MaLi_GAN/plot_acc.py
+++ MaLi_GAN/plot_acc.py
@@ -2,10 +2,7 @@ import matplotlib.pyplot as plt
 import cPickle 
 import numpy as np
 
-acc2 = cPickle.load(open('./accuracies.txt'))
-acc3 = cPickle.load(open('./accuracies3.txt'))
-print acc2, acc3
-acc = np.concatenate((acc2, acc3))
+acc = cPickle.load(open('./pretrain_losses.txt'))
 
 plt.plot(range(len(acc)), acc)
 
