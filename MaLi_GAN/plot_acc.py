676035383e30bcb49d303843a45318528edaeb2b
Brendan Corcoran <BrendanCorcoran@DN525av0.SUNet>
2017-03-17 17:40:26 -0700

    updates to bmg

--- MaLi_GAN/plot_acc.py
+++ MaLi_GAN/plot_acc.py
@@ -3,7 +3,7 @@ import cPickle
 import numpy as np
 
 acc = cPickle.load(open('./pretrain_perplexities_eval.txt'))
-acc2 = cPickle.load(open('./eval_perps.txt'))
+acc2 = cPickle.load(open('./20170317-234848/eval_perps.txt'))
 acc = np.concatenate((acc, acc2))
 
 plt.plot(range(len(acc)), acc)
