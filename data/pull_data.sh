d1964eaa476240458699c9018c6647ee64201bec
Christina Hung <cjh@cs.stanford.edu>
2017-03-18 06:13:47 +0000

    fixed data_preprocess

---
+++ data/pull_data.sh
@@ -0,0 +1,2 @@
+wget -c 'https://www.dropbox.com/s/rnf9ms23smxnqp2/trimmed_glove_vectors.npy?dl=0' -O glove/trimmed_glove_vectors.npy
+wget -c 'https://www.dropbox.com/s/58trhkgb8e29t2b/trimmed_word_lexicon.txt?dl=0' -O glove/trimmed_word_lexicon.txt
