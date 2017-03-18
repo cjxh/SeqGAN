490afd62a62c69754814b95d69a111779ed339d6
Brendan Corcoran <BrendanCorcoran@DN525av0.SUNet>
2017-03-17 18:56:01 -0700

    changes to bbasic

--- MaLi_GAN/plot_acc.py
+++ MaLi_GAN/plot_acc.py
@@ -6,7 +6,7 @@ import numpy as np
 # acc2 = cPickle.load(open('./20170317-234848/eval_perps.txt'))
 # acc = np.concatenate((acc, acc2))
 
-acc = cPickle.load(open('./20170318-004737/pretrain_perplexities.txt'))
+acc = cPickle.load(open('./20170318-011742/eval_perps.txt'))
 
 plt.plot(range(len(acc)), acc)
 
