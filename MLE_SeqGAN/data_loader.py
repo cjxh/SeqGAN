8072cb0296974fde6e4e5ba3ed5e14bf44f5e363
Christina Hung <cjh@cs.stanford.edu>
2017-03-19 07:20:21 +0000

    some stuff

---
+++ MLE_SeqGAN/data_loader.py
@@ -0,0 +1,107 @@
+import numpy as np
+import math
+from tqdm import tqdm
+
+UNK = 0
+START = 1 
+END = 2
+
+class DataLoader(object):
+    def __init__(self, N, batch_size, is_synthetic, data_file, lexicon = {}):
+        self.batch_size = batch_size
+        self.max_length = N
+        self.token_stream = []
+        self.mask_sequence_stream = []
+        self.pointer = 0
+        self.SYNTHETIC = is_synthetic
+        self.sentences = []
+        self.lexicon = lexicon
+        if not is_synthetic:
+            self.load_data(data_file)
+        else:
+            self.load_syn_data(data_file)
+        self.mini_batch(self.batch_size)
+
+    '''
+    normalizes sentence length to max sentence length
+    shuffles indices of data to help randomize minibatches
+    '''
+    def pre_process_sentences(self):
+        temp_token_stream = []
+        num_sentences = len(self.token_stream)
+        mask_sequence_stream = []
+        print "Padding sentences in token_stream to " + str(self.max_length)  + "..."
+        for sentence in tqdm(self.token_stream):
+            mask_sequence = [True] * len(sentence)
+            if len(sentence) < self.max_length:
+                for i in range(self.max_length - len(sentence)):
+                    sentence.append(END)
+                    mask_sequence.append(False)
+            mask_sequence_stream.append(mask_sequence)
+            temp_token_stream.append(sentence)
+        return temp_token_stream, mask_sequence_stream
+
+    def load_syn_data(self, data_file):
+        with open(data_file, 'r') as f:
+            print "Parsing synthetic sentences in " + str(data_file) + "..."
+            for line in tqdm(f):
+                print "Doing this loop"
+                if self.SYNTHETIC:
+                    parsed_line = []
+                    line = line.strip()
+                    line = line.split()
+                    parsed_line = [int(num) for num in line]
+                    if len(parsed_line) <= 20:
+                        self.token_stream.append(parsed_line)
+            self.token_stream, self.mask_sequence_stream = self.pre_process_sentences()
+
+    def load_data(self, data_file):
+        with open(data_file, 'r') as f:
+            print "Parsing sentences in " + str(data_file) + "..."
+            parsed_line = []
+            word_ct = 0
+            sentence_ct = 0
+            self.num_unk = 0
+            for word in tqdm(f):
+                word_ct += 1
+                word = word.strip()
+                if word in self.lexicon.keys():
+                    parsed_line.append(self.lexicon[word])
+                else:
+                    self.num_unk += 1
+                    parsed_line.append(UNK)
+                if word == '<END>':
+                    sentence_ct += 1
+                    self.token_stream.append(parsed_line)
+                    parsed_line = []
+        print "percent of unkowns: " + str((self.num_unk * 1.0) / word_ct)
+        print "num_sentences: " + str(sentence_ct)
+        self.token_stream, self.mask_sequence_stream = self.pre_process_sentences()
+
+    def next_batch(self):
+        ret = self.mini_batches[self.pointer]
+        ret_mask = self.mini_batches_mask[self.pointer]
+        self.pointer = (self.pointer + 1) % self.num_batch
+        return ret, ret_mask
+
+    def reset_pointer(self):
+        self.pointer = 0
+
+    def shuffle_sentences(self):
+        num_sentences = len(self.token_stream)
+        shuffle_idx = np.random.permutation(np.arange(num_sentences))
+        shuffled_data = []
+        shuffled_mask = []
+        for i in shuffle_idx:
+            shuffled_data.append(self.token_stream[i])
+            shuffled_mask.append(self.mask_sequence_stream[i])
+        return shuffled_data, shuffled_mask
+
+    def mini_batch(self, batch_size):
+        self.num_batch = len(self.token_stream) / batch_size
+        shuffled_stream, shuffled_mask = self.shuffle_sentences()
+        shuffled_stream = shuffled_stream[:self.num_batch * batch_size]
+        shuffled_mask = shuffled_mask[:self.num_batch * batch_size]
+        self.mini_batches = np.split(np.array(shuffled_stream), self.num_batch, 0)
+        self.mini_batches_mask = np.split(np.array(shuffled_mask), self.num_batch, 0)
+        self.pointer = 0
---
+++ MLE_SeqGAN/pretrain_real.py
@@ -0,0 +1,100 @@
+import model
+import numpy as np
+import tensorflow as tf
+import random
+#from gen_dataloader import Gen_Data_loader, Likelihood_data_loader
+from data_loader import DataLoader as dl
+from target_lstm import TARGET_LSTM
+import cPickle, yaml
+import time, os
+from config import GenConfig
+TIME = time.strftime('%Y%m%d-%H%M%S')
+
+#########################################################################################
+#  Generator  Hyper-parameters
+#########################################################################################
+CONFIG = GenConfig()
+CONFIG.PRE_EPOCH_NUM = 50
+
+##########################################################################################
+positive_file = '../data/preprocessed_data/train.txt'
+eval_file = '../data/preprocessed_data/eval.txt'
+
+generated_num = 10000
+
+class PoemGen(model.LSTM):
+    def g_optimizer(self, *args, **kwargs):
+        return tf.train.AdamOptimizer()  # ignore learning rate
+
+
+def get_trainable_model(num_emb):
+    return PoemGen(num_emb, CONFIG.BATCH_SIZE, 300, CONFIG.HIDDEN_DIM, CONFIG.SEQ_LENGTH, \
+        CONFIG.START_TOKEN)
+
+
+def target_loss(sess, target_lstm, data_loader):
+    supervised_g_losses = []
+    data_loader.reset_pointer()
+
+    for it in xrange(data_loader.num_batch):
+        batch = data_loader.next_batch()
+        g_loss = sess.run(target_lstm.pretrain_loss, {target_lstm.x: batch})
+        supervised_g_losses.append(g_loss)
+
+    return np.mean(supervised_g_losses)
+
+
+def pre_train_epoch(sess, trainable_model, data_loader):
+    supervised_g_losses = []
+    data_loader.reset_pointer()
+
+    for it in xrange(data_loader.num_batch):
+        batch, mask = data_loader.next_batch()
+        _, g_loss, g_pred = trainable_model.pretrain_step(sess, batch, mask)
+        supervised_g_losses.append(g_loss)
+
+    print '>>>> generator train loss:', np.mean(supervised_g_losses)
+    loss = np.mean(supervised_g_losses)
+    return loss, np.exp(loss)
+
+def main():
+    #random.seed(CONFIG.SEED)
+    #np.random.seed(CONFIG.SEED)
+
+    #assert CONFIG.START_TOKEN == 0
+    lexicon = cPickle.load(open('../data/glove/trimmed_word_lexicon.p', 'r'))
+    vocab_size = len(lexicon.keys())
+    seqlen = 35
+    batch_size = 64 
+
+    #gen_data_loader = DataLoader(CONFIG.BATCH_SIZE)
+    gen_data_loader = dl(seqlen, batch_size, False, positive_file, lexicon)
+    #likelihood_data_loader = DataLoader(CONFIG.BATCH_SIZE)
+    eval_dl = dl(seqlen, batch_size, False, eval_file, lexicon)
+
+    generator = get_trainable_model(vocab_size)
+
+    #target_params = cPickle.load(open('save/target_params.pkl'))
+    #target_lstm = TARGET_LSTM(vocab_size, 64, 32, 32, 20, 0, target_params)
+
+    saver = tf.train.Saver()
+
+    config = tf.ConfigProto()
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.5
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    sess.run(tf.global_variables_initializer())
+
+    #  pre-train generator
+    print 'Start pre-training...'
+    
+    for epoch in xrange(CONFIG.PRE_EPOCH_NUM):
+        print 'pre-train epoch:', epoch
+        loss = pre_train_epoch(sess, generator, gen_data_loader)
+        
+
+    sess.close()
+
+
+if __name__ == '__main__':
+    main()
